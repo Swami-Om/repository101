4d8f3bd5648e981b3b36992d5b6fafffcf4d380e
Swami-Om <119023161+Swami-Om@users.noreply.github.com>
2022-11-25 08:14:32 +0530

    Update python.py
    This is python file

--- python.py
+++ python.py
@@ -1,1 +1,9 @@
 print("Hello Github!")
+students = ["Hariom", "Atharva"]
+def studentname():
+  for i in students:
+    printf("The students creating this file are : {i}")
+
+    
+    
+studentname()
